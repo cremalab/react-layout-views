ad2c6e99b06942df6d2c60a81f8cd910df8c992f
Rob LaFeve <roblafeve@gmail.com>
2017-11-05 21:31:49 -0600

    Update web types

--- @types/web.d.ts
+++ @types/web.d.ts
@@ -1,28 +1,29 @@
 import { StyledProps } from 'styled-components'
+import { HtmlHTMLAttributes, CSSLength } from 'react'
 
-interface Layout extends StyledProps<any> {
-  grow: boolean | number
-  spacing: string
-  spacingInfo: {
+interface Layout extends HtmlHTMLAttributes<any> {
+  grow?: boolean | number
+  spacing?: CSSLength
+  spacingInfo?: {
     value: number
-    unit: string
+    unit: CSSLength
   }
-  horizontal: boolean
-  center: boolean
-  top: boolean
-  right: boolean
-  bottom: boolean
-  left: boolean
+  horizontal?: boolean
+  center?: boolean
+  top?: boolean
+  right?: boolean
+  bottom?: boolean
+  left?: boolean
 }
 
-interface Section extends StyledProps<any> {
-  parentProps: Layout
-  grow: boolean | number
-  center: boolean
-  centerVertical: boolean
-  centerHorizontal: boolean
-  top: boolean
-  right: boolean
-  bottom: boolean
-  left: boolean
+interface Section extends HtmlHTMLAttributes<any> {
+  parentProps?: Layout
+  grow?: boolean | number
+  center?: boolean
+  centerVertical?: boolean
+  centerHorizontal?: boolean
+  top?: boolean
+  right?: boolean
+  bottom?: boolean
+  left?: boolean
 }
--- src/dist/index.tsx
+++ src/dist/index.tsx
@@ -1,7 +1,13 @@
 import React, { Children, cloneElement, SFC } from 'react'
-import styled, { css, StyledComponentClass } from 'styled-components'
+import styled, { css, ThemedStyledFunction, StyledComponentClass } from 'styled-components'
 import CSSLength from 'css-length'
 import { Layout as LayoutProps, Section as SectionProps } from '../../@types/web'
+
+function withProps<U>() {
+  return <P, T, O>(
+    fn: ThemedStyledFunction<P, T, O>
+  ) => fn as ThemedStyledFunction<P & U, T, O & U>
+}
 
 export const Layout: SFC<LayoutProps> = ({style, ...rest}) => {
   const spacingInfo = new CSSLength(rest.spacing)
@@ -23,7 +29,7 @@ Layout.defaultProps = {
   spacing: '0px'
 }
 
-const LayoutWrapper = styled.div`
+const LayoutWrapper = withProps<LayoutProps>()(styled.div)`
   ${({ grow, horizontal }: LayoutProps) => {
     return css`
       overflow: hidden;
@@ -33,7 +39,7 @@ const LayoutWrapper = styled.div`
   }}
 `
 
-const LayoutInner = styled.div`
+const LayoutInner = withProps<LayoutProps>()(styled.div)`
   ${({
     horizontal, spacingInfo, grow,
     center, top, right, bottom, left
@@ -43,7 +49,7 @@ const LayoutInner = styled.div`
       ${(grow || horizontal) && `display: flex;`}
       ${(grow || horizontal) && `flex: ${typeof grow === 'number' ? grow : 1};`}
       flex-direction: ${horizontal ? 'row' : 'column'};
-      margin: ${-(spacingInfo.value / 2) + spacingInfo.unit};
+      margin: ${(-(spacingInfo.value / 2)).toString() + spacingInfo.unit};
       ${horizontal 
         ? css`
           ${center && `align-items: center; justify-content: center;` }
@@ -64,14 +70,14 @@ const LayoutInner = styled.div`
   }}
 `
 
-export const Section = styled.div`
+export const Section = withProps<SectionProps>()(styled.div)`
   ${(props: SectionProps) => {
     const { grow, center, centerVertical, centerHorizontal, top, right, bottom, left } = props
     const { horizontal, spacingInfo } = props.parentProps
     return css`
       display: flex;
       flex-direction: column;
-      padding: ${(spacingInfo.value / 2) + spacingInfo.unit};
+      padding: ${(spacingInfo.value / 2).toString() + spacingInfo.unit};
       ${grow             && `flex: ${typeof grow === 'number' ? grow : 1 };` }
       ${center           && `align-items: center; justify-content: center;`  }
       ${centerVertical   && `justify-content: center;`                       } 
